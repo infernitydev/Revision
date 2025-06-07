a3667a9fde00a073afb0946c2f8ff7f2f0f90ac9
Whirling <infernity@infernity.dev>
2025-06-07 14:57:48 -0600

    change the controls

--- addons/revision/scenes/editor/graphedit.gd
+++ addons/revision/scenes/editor/graphedit.gd
@@ -1,5 +1,21 @@
 class_name MPMapScriptGraph extends GraphEdit
 
+func _ready() -> void:
+	var hbox = get_menu_hbox()
+	for item in hbox.get_children():
+		if item is Control:
+			match item.tooltip_text:
+				"Toggle the visual grid.":
+					item.hide()
+				"Change the snapping distance.":
+					item.tooltip_text = "Change the grid distance."
+					item.step = 10
+					item.min_value = 10
+					item.max_value = 40
+					item.suffix = "px"
+				_:
+					pass
+
 func connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	print("-- connection request --")
 	print(from_node)
