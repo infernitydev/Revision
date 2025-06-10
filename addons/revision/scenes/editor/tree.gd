8a8996f62d60e183ccae034fa22c0d2745e4cd9e
diginist <xanmangaming8@gmail.com>
2025-06-09 21:00:22 -0500

    git plugin and drag-and-drop init

--- addons/revision/scenes/editor/tree.gd
+++ addons/revision/scenes/editor/tree.gd
@@ -1,5 +1,7 @@
 extends Tree
 
+var selected_item = null
+
 func _ready():
 	var tree = self
 	var root = tree.create_item()
@@ -10,3 +12,20 @@ func _ready():
 	outputs.set_text(0, "Outputs")
 	var subchild1 = tree.create_item(inputs)
 	subchild1.set_text(0, "Subchild1")
+	item_mouse_selected.connect(drag_item)
+
+func drag_item(pos,button):
+	if button == MOUSE_BUTTON_LEFT:
+		selected_item = get_selected()
+		var preview = Label.new()
+		preview.text = selected_item.get_text(0)
+		force_drag(selected_item, preview)
+		preview.tree_exited.connect(print.bind("Bye"))
+		
+#these are just for testing
+func _can_drop_data(at_position: Vector2, data: Variant):
+	return true
+
+func _drop_data(at_position: Vector2, data: Variant):
+	print("dropped")
+	print(data)
